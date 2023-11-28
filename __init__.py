d021d8821b1d4da466dbc4fca482685a9c62663b
Bruno Madeira <bmad.works@gmail.com>
2023-11-28 15:17:31 +0000

    remove older web/extensions folder

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+import os, shutil
 import __main__
+
+# remove old directory
+extentions_folder = os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)),
+                                 "web" + os.sep + "extensions" + os.sep + "ZZZ-Bmad-DirtyUndoRedo")
+if os.path.isdir(extentions_folder):
+    shutil.rmtree(extentions_folder)
+
+# set web dir
 WEB_DIRECTORY = "js"
 print('\033[92mBmad-DirtyUndoRedo Loaded.\033[0m')
+
+
+
